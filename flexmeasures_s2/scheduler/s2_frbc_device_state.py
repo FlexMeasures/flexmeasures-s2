44984609ee2eabcb27b9d6c8eeb9f42c2fba2b23
Vlad Iftime <vladiftime60@gmail.com>
2025-08-01 02:05:20 +0200

    Enhance type safety in S2FrbcDeviceState by allowing arbitrary types in model configuration, improving flexibility for system descriptions.
    Signed-off-by: Vlad Iftime <vladiftime60@gmail.com>

--- flexmeasures_s2/scheduler/s2_frbc_device_state.py
+++ flexmeasures_s2/scheduler/s2_frbc_device_state.py
@@ -4,7 +4,10 @@ from s2python.frbc import (
 
 from typing import List
 from pydantic import BaseModel
+from pydantic.config import ConfigDict
 
 
 class S2FrbcDeviceState(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     system_descriptions: List[FRBCSystemDescription]
