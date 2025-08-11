9770f368d6ebe546c7fde5dddd260d90707da8c3
Vlad Iftime <vladiftime60@gmail.com>
2025-08-11 11:43:42 +0200

    Refactor: Fixed pydantic version error for BaseModel arbitrart_types
    Signed-off-by: Vlad Iftime <vladiftime60@gmail.com>

--- flexmeasures_s2/profile_steering/common/pydantic_base.py
+++ flexmeasures_s2/profile_steering/common/pydantic_base.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
-from pydantic.config import ConfigDict
 
 
 class FlexMeasuresBaseModel(BaseModel):
     """Base model for FlexMeasures S2 with arbitrary types allowed."""
 
-    model_config = ConfigDict(arbitrary_types_allowed=True)
+    class Config:
+        arbitrary_types_allowed = True
