283e96b320b8b1ef6d64a2b9f87d22b6a74a71ac
F.N. Claessen <felix@seita.nl>
2025-12-07 22:08:44 +0100

    refactor: move S2FlaskWSServerSync from flexmeasures to flexmeasures-s2 plugin
    Signed-off-by: F.N. Claessen <felix@seita.nl>

--- flexmeasures_s2/__init__.py
+++ flexmeasures_s2/__init__.py
@@ -10,7 +10,9 @@ FlexMeasures registers the BluePrint objects it finds in here.
 
 from importlib_metadata import version, PackageNotFoundError
 
-from flask import Blueprint
+from flask import Blueprint, current_app as app
+
+from flexmeasures.ws import sock
 
 from .utils import ensure_bp_routes_are_loaded_fresh
 
@@ -26,11 +28,13 @@ except PackageNotFoundError:
 
 # API
 flexmeasures_s2_api_bp: Blueprint = Blueprint(
-    "flexmeasures-s2 API", __name__, url_prefix="/flexmeasures-s2/api"
+    "flexmeasures-s2 API", __name__, url_prefix="/s2"
 )
 ensure_bp_routes_are_loaded_fresh("api.somedata")
 from flexmeasures_s2.api import somedata  # noqa: E402,F401
 
+
+s2_ws = somedata.S2FlaskWSServerSync(app=app, sock=sock)  # noqa: F841
 
 # Use as follows:
 # from flexmeasures import Sensor
